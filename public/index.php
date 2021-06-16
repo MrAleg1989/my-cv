029a61e671c540eab791c121dd35e703ac640a1c
Oleg <oleg.yalam@gmail.com>
2021-06-16 19:17:10 +0300

    Update my CV homework

--- public/index.php
+++ public/index.php
@@ -6,5 +6,9 @@
 <body>
 	<h1>Oleg Ialama</h1>
 	<p>Email oleg.yalam@gmail.com</p>
+	<div> Telephone: 79344219 </div>
+	<div>
+        <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
+    </div>
 </body>
 </html>
